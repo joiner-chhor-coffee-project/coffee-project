6feb2ca4fedea7b9086696c385564d41c5b71871
Austin Joiner <austin.j.joiner1@gmail.com>
2023-03-02 00:25:51 -0500

    added in functioning components to make all option make all roasts appear and search coffee name only display coffees matching the roasts.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function renderCoffee(coffee) {
+function renderCoffee(coffee) { // this function pulls from the array and wraps it in a div and assign name the header element and roast the p element
     var html = '<div class="coffee">';
     // html += '<td>' + coffee.id + '</td>';
     html += '<h3>' + coffee.name + '</h3>';
@@ -9,63 +9,73 @@ function renderCoffee(coffee) {
 
     return html;
 }
-
-function renderCoffees(coffees) {
-    var html = '<div class="col-lg-3 col-md-4 col-sm-6">';
+//
+//
+//
+function renderCoffees(coffees) { // this function loops html through nesting it in a parent div with a column and a conditional making 3 coffees group up inside the div.
+    var html = '<div class="col-lg-6 col-md-6 col-sm-6"><!-- start of loop-->'; // start of loop
     for(var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
         if (i % 3 == 2) {
-            html += "</div><div class='col-lg-4 col-md-3 col-sm-6'>";
+            html += "</div><div class='col-lg-6 col-md-6 col-sm-6'>";
         }
-        html += renderCoffees (coffees[i]);
+         //html += renderCoffees (coffees[i]);
 
     }
-    html += '</div>';
+    html += '</div><!--end of loop-->'; // end of loop
 
     return html;
 }
-
-function updateCoffees(e) {
+//
+//
+//
+function updateCoffees(e) { // function for roast drop down.
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-
-    //
-    //
-
-    //
-
     if (selectedRoast === 'All') { // displays all roast types
         filteredCoffees = coffees;
+        tbody.innerHTML = renderCoffees(coffees); // this fixes the all option making it where the var 'tbody.innerHTML' calls on '#coffee' and makes it equal its original value to bring back the whole list.
     } else {
-        //
-
         coffees.forEach(function (coffee) {
             if (coffee.roast === selectedRoast) {
                 filteredCoffees.push(coffee);
             }
         });
-
         tbody.innerHTML = renderCoffees(filteredCoffees);
-
     }
 }
+//
+//
+//
 
-function coffeeName(e) {
+
+function coffeeName(e) { // function for searching for a coffee
     //e.preventDefault();
-    var selectedName = coffeeSearch.value.toLowerCase();
-    var filteredCoffeesName = [];
-    //
+    var selectedName = coffeeSearch.value.toLowerCase(); // change coffee search input to lowercase
+    var filteredCoffees = [];
 
-    coffees.forEach(function (coffee) {
-        if (coffee.name.toLowerCase().includes(selectedName)) {
-            filteredCoffeesName.push(coffee);
-        }
-    });
-     tbody.innerHTML = renderCoffees(filteredCoffeesName);
+    if (roastSelection.value === 'All') { // this checks if roast is set to all if it is all coffees are filtered
+        filteredCoffees = coffees;
+    } else {
+        //using the filter method we creat a new array of coffee that matches the active roast and stores it inside filteredCoffees array.
+        filteredCoffees = coffees.filter(function (coffee) {
+            return coffee.roast === roastSelection.value;
+        });
+    }
+
+    //filters to create another new array of coffees matching the selected name.
+     var filteredCoffeesName = filteredCoffees.filter(function (coffee) {
+            return coffee.name.toLowerCase().includes(selectedName);
+     });
+    // adds the coffee to the table that was chosen.
+    tbody.innerHTML = renderCoffees(filteredCoffeesName);
 }
 
 
+//
+//
+//
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -83,17 +93,17 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
+//
+//
+//
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 var coffeeSearch = document.querySelector('#coffee-search');
 
-
 tbody.innerHTML = renderCoffees(coffees);
 
-
-coffeeSearch.addEventListener('keydown', coffeeName);
+coffeeSearch.addEventListener('keyup', coffeeName); // keydown makes it delay the change this is more abrupt.
 roastSelection.addEventListener('change', updateCoffees);
 
 
